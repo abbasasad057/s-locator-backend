d28dd20ea0d8a76186a4ddec79b59a443998fd4a
abdullah abbas <abdullahalhoothy92@gmail.com>
2025-10-29 16:08:07 -0400

    fix: update cost calculation multiplier for accurate cost estimation

--- cost_calculator.py
+++ cost_calculator.py
@@ -133,6 +133,6 @@ async def calculate_cost(req: ReqFetchDataset, text_search:bool=False):
             total_api_calls = 500 *0.5 # arbtrary discount
             
         cost = (total_api_calls / 1000) * COST_PER_1000_CALLS
-        cost *= 20
+        cost *= 50
             
         return ResCostEstimate(cost=cost, api_calls=total_api_calls)
